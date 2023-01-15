0a34239743daffa885a8cfed58002534556a41de
Влад <vladearth7@gmail.com>
2023-01-15 03:05:15 +0200

    Initialized books app & add book model

---
+++ books/models.py
@@ -0,0 +1,18 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "HR", _("Hard")
+        SOFT = "ST", _("Soft")
+
+    title = models.CharField(max_length=255, null=False, blank=False)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=2,
+        choices=Cover.choices
+    )
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    daily_fee = models.DecimalField(label="USD")
