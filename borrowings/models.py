65552e29e12480dd694fb982415ee45826798532
Влад <vladearth7@gmail.com>
2023-04-05 13:29:12 +0300

    Update borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,10 +1,23 @@
-from django.core.validators import MinValueValidator
+from django.conf import settings
 from django.db import models
+from books.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
-    expected_return_date = models.DateField()
-    actual_return_date = models.DateField()
-    book_id = models.IntegerField(validators=[MinValueValidator(1)])
-    user_id = models.IntegerField(validators=[MinValueValidator(1)])
+    borrow_date = models.DateField(auto_now_add=True)
+    expected_return_date = models.DateField(default=3)
+    actual_return_date = models.DateField(blank=True, null=True)
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+
+    def __str__(self):
+        return f"{self.book.title} was borrow {self.borrow_date} " \
+               f"expected return {self.expected_return_date}"
